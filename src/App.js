e2df92ab9326f6d07b035025d0e8650051096f35
Baraka Mahili <georgesmahili@gmail.com>
2017-08-27 14:30:06 -0400

    added a drawer functionality to only open the app drawer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import AppBar from 'material-ui/AppBar';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
   render() {
     return (
       <div className="App">
         <AppBar
           title="Baraka Mahili React"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
+          onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
         />
+        <Drawer open={this.state.open}>
+          <MenuItem>Menu Item</MenuItem>
+          <MenuItem>Menu Item 2</MenuItem>
+        </Drawer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
